7f8e38fd16d8717899fe935b8cecbe0f0d352c33
noahsusimposter <110932966+noahsusimposter@users.noreply.github.com>
2023-02-01 14:31:20 -0500

    calc.lua
    fr

---
+++ calc.lua
@@ -0,0 +1,25 @@
+local first
+local second
+local awnser
+local operation
+local awnsered = false
+
+while true do
+    if awnsered == false then
+        io.write(“Operation (A, S, M, D)”)
+        operation = io.read
+    end
+wait(0.1)
+end
+
+while true do
+io.write(“First number”)
+local first = io.read
+wait(0.1)
+end
+
+while true do
+io.write(“Second number”)
+local second = io.read
+wait(0.1)
+end
